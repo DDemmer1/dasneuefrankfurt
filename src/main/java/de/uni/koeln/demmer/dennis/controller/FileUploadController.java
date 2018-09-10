d6e1bffc338e8800351b842389f8b4771523720a
DDemmer1 <Dennis.Demmer@web.de>
2018-09-10 11:25:42 +0200

    added upload controller

---
+++ src/main/java/de/uni/koeln/demmer/dennis/controller/FileUploadController.java
@@ -0,0 +1,187 @@
+package de.uni.koeln.demmer.dennis.controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import Cheiron.Cheiron;
+import DnfWrapper.DNFUtil;
+import de.uni.koeln.demmer.dennis.controller.storage.StorageFileNotFoundException;
+import de.uni.koeln.demmer.dennis.controller.storage.StorageService;
+import de.uni.koeln.demmer.dennis.model.autocorrect.Util.*;
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.w3c.dom.Document;
+import org.w3c.dom.Text;
+
+
+@CrossOrigin(maxAge = 3600)
+@RestController
+public class FileUploadController {
+
+    private final StorageService storageService;
+
+    @Autowired
+    public FileUploadController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
+//    @GetMapping("/")
+//    public String listUploadedFiles(Model model) throws IOException {
+//
+//        model.addAttribute("files", storageService.loadAll().map(
+//                path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
+//                        "serveFile", path.getFileName().toString()).build().toString())
+//                .collect(Collectors.toList()));
+//
+//        return "uploadForm";
+//    }
+
+//    @GetMapping("/files/{filename:.+}")
+//    @ResponseBody
+//    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+//
+//        Resource file = storageService.loadAsResource(filename);
+//        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+//                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+//    }
+
+    @PostMapping("/uploadtxt")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file,
+            RedirectAttributes redirectAttributes) throws IOException {
+
+//        storageService.store(file);
+//        redirectAttributes.addFlashAttribute("message",
+//                "You successfully uploaded " + file.getOriginalFilename() + "!");
+//
+//        File text =storageService.load(file.getOriginalFilename()).toFile();
+
+//        File convFile = null;
+//        try {
+//            convFile = new File(file.getOriginalFilename());
+//            convFile.createNewFile();
+//            FileOutputStream fos = new FileOutputStream(convFile);
+//            fos.write(file.getBytes());
+//            fos.close();
+//        } catch (Exception e){
+//
+//        }
+
+        File convFile = FileConverter.convertToFile(file);
+
+        String input ="";
+        try {
+            input = TextUtil.readFile(convFile.getPath(), StandardCharsets.ISO_8859_1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        TextPreProcessor prePro = new TextPreProcessor(input);
+        String text = TextPreProcessor.getText();
+
+
+        List<Token> tokenList = Tokenizer.tokenize(text);
+
+        TokenProcessor tokenProcessor = new TokenProcessor();
+        tokenList = tokenProcessor.process(tokenList);
+
+        XMLBuilder xmlBuilder = new XMLBuilder(tokenList);
+        Document xml = xmlBuilder.buildXML();
+
+        return TextUtil.readFile(xmlBuilder.getResult().getPath(),StandardCharsets.UTF_8);
+
+    }
+
+
+
+    @PostMapping("/uploadzip")
+    public ResponseEntity<Resource> handleZIPUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) throws IOException {
+
+        FileUtils.cleanDirectory(new File("data/ner"));
+
+        File convFile = FileConverter.convertToFile(file);
+        List<File> unpacked = FileConverter.unpackZIP(convFile);
+
+
+        List<File> results = new ArrayList<>();
+
+
+        int i = 0;
+        for (File unpackedFile: unpacked) {
+            String text = TextUtil.readFile(unpackedFile.getPath(),StandardCharsets.ISO_8859_1);
+
+            //NER
+            try {
+                DNFUtil.clearCheiron();
+                Cheiron.findEntitys(text);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            File xmloutput = new File("data/out/text.xml");
+            File toZIP = new File("data/ner/text"+i+".xml");
+            Files.copy(xmloutput.toPath(), toZIP.toPath());
+            i++;
+        }
+
+        for (int j = 0; j < i; j++) {
+            results.add(new File("data/ner/text"+j+".xml"));
+        }
+
+
+
+        System.out.println(results.toString());
+        File resultZIP = FileConverter.convertTozip(results);
+
+        HttpHeaders headers = new HttpHeaders(); headers.add(HttpHeaders.CONTENT_DISPOSITION
+                ,"attachment; filename=result.zip");
+        Path path = Paths.get(resultZIP.getAbsolutePath());
+        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(resultZIP.length())
+                .contentType(MediaType.parseMediaType("application/zip"))
+                .body(resource);
+    }
+
+
+
+
+
+
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+        return ResponseEntity.notFound().build();
+    }
+
+
+
+
+}
---
+++ src/main/java/de/uni/koeln/demmer/dennis/model/autocorrect/Util/FileConverter.java
@@ -0,0 +1,91 @@
+package de.uni.koeln.demmer.dennis.model.autocorrect.Util;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
+public class FileConverter {
+
+
+    public static File convertToFile(MultipartFile file) {
+        File convFile = null;
+        try {
+            convFile = new File(file.getOriginalFilename());
+            convFile.createNewFile();
+            FileOutputStream fos = new FileOutputStream(convFile);
+            fos.write(file.getBytes());
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return convFile;
+    }
+
+    public static File convertTozip(List<File> files) {
+        try {
+            FileOutputStream fos = new FileOutputStream("result.zip");
+            ZipOutputStream zos = new ZipOutputStream(fos);
+            for (File file : files) {
+                addToZipFile(file, zos);
+            }
+            zos.close();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new File("result.zip");
+    }
+
+    private static void addToZipFile(File file, ZipOutputStream zos) throws FileNotFoundException, IOException {
+
+        System.out.println("Writing '" + file.getName() + "' to zip file");
+
+        FileInputStream fis = new FileInputStream(file);
+        ZipEntry zipEntry = new ZipEntry(file.getName());
+        zos.putNextEntry(zipEntry);
+
+        byte[] bytes = new byte[1024];
+        int length;
+        while ((length = fis.read(bytes)) >= 0) {
+            zos.write(bytes, 0, length);
+        }
+
+        zos.closeEntry();
+        fis.close();
+    }
+
+
+    public static List<File> unpackZIP(File fileZip) throws IOException {
+
+        List<File> unpacked = new ArrayList<>();
+        byte[] buffer = new byte[1024];
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(fileZip));
+        ZipEntry zipEntry = zis.getNextEntry();
+        while (zipEntry != null) {
+            String fileName = zipEntry.getName();
+            File newFile = new File(fileName);
+            unpacked.add(newFile);
+            FileOutputStream fos = new FileOutputStream(newFile);
+            int len;
+            while ((len = zis.read(buffer)) > 0) {
+                fos.write(buffer, 0, len);
+            }
+            fos.close();
+            zipEntry = zis.getNextEntry();
+        }
+        zis.closeEntry();
+        zis.close();
+
+
+        return unpacked;
+    }
+
+}
